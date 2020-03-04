514997e2f5b1a91a24c90dfdbcc17d0fd4d4cd48
Niraj <amatyaraj@gmail.com>
2020-03-04 14:26:41 +1100

    added new and create controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -8,11 +8,15 @@ class ConversationsController < ApplicationController
     end
 
     def new
-
+        @recipients = User.all - [current_user]
     end
 
     def create
-
+        recipient = User.find(params[:user_id])
+        receipt   = current_user.send_message(recipient, params[:body], params[:subject])
+        redirect_to conversation_path(receipt.conversation)
     end
+
+    
     
 end
