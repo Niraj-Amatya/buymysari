c8f1d3053f5c0217e209c86b4d472e87bdb1a30d
Niraj <amatyaraj@gmail.com>
2020-03-12 18:32:39 +1100

    changed heading

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,9 @@
 class Listing < ApplicationRecord
   # category as enum, user as a seller
   enum category: { party: 0, casual: 1 }
+  # these are the fields that are acceptable
   validates :title, :price, :color, :fabric, :description, :category, presence: true
+  # listing belongs to user, so listing will have user_id as a foreign key
   belongs_to :user
   has_one :order
   has_one_attached :picture
