5aeeb1965c0d96eec2aa956a2e616217de2f171d
Niraj <amatyaraj@gmail.com>
2020-03-12 13:24:26 +1100

    added some seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 for i in 1..10
+    first_name = Faker::Name.first_name,
+    last_name = Faker::Name.last_name,
     user = User.create(
         email: "test+#{i}@test.com",
-        password: "Testing123"
+        password: "Testing123",
+        first_name: first_name,
+        last_name: last_name,
+        username: "#{first_name}+ #{last_name}",
+        address: Faker::Address.city,
+        about: Faker::Lorem.paragraph
     )
     puts "Created user #{user.id}"
 
